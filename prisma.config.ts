7ddad8b5c642231f2266c4f888e879a1f1c4fb1b
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-03-21 17:37:52 +0600

    feat: removed the see file &* installed the prisma sutdio for adding products

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,9 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    // CLI commands like 'db push' need the Direct (non-pooled) connection
     url: process.env.DIRECT_URL,
+  },
+  migrations: {
+    seed: "tsx prisma/seed.ts",
   },
 });
