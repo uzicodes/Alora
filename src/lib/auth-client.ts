16127d10e6ec5bb8d3004473dd45af906da4cb67
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-03-29 23:19:38 +0600

    feat: installed better-auth infra package and added the client & server page

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,11 @@
 import { createAuthClient } from "better-auth/react";
+import { sentinelClient } from "@better-auth/infra/client";
 
 export const authClient = createAuthClient({
   // Use your local dev URL for now. When you deploy to Vercel, this will need to be your production URL.
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || "https://aloraa.vercel.app/",
+  plugins: [sentinelClient()],
 });
 
-// Export the hooks so you can use them anywhere in your app
+
 export const { signIn, signUp, signOut, useSession } = authClient;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma"; 
 import { PrismaClient } from "../generated/prisma"; 
+import { dash } from "@better-auth/infra";
 
 const prisma = new PrismaClient();
 
@@ -17,4 +18,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [dash()],
 });
