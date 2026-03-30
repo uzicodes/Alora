69b09a215892b5be57c404d528a150997a6620a4
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-03-30 17:06:36 +0600

    fix: CORS error fixed with the auth-client & fixed NEXT_PUBLIC_APP_URL in .env

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,11 +1,11 @@
 import { createAuthClient } from "better-auth/react";
-import { sentinelClient } from "@better-auth/infra/client";
+import { sentinelClient } from "@better-auth/infra/client"; 
 
 export const authClient = createAuthClient({
-  // Use your local dev URL for now. When you deploy to Vercel, this will need to be your production URL.
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "https://aloraa.vercel.app/",
-  plugins: [sentinelClient()],
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  plugins: [
+    sentinelClient() 
+  ]
 });
-
 
 export const { signIn, signUp, signOut, useSession } = authClient;
