372c53d89cfb6670b8471046b0e9665197907b75
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-04-14 22:41:17 +0600

    feat: UI tweaks on the welcome email templete

--- src/lib/email-templetes/WelcomeEmail.ts
+++ src/lib/email-templetes/WelcomeEmail.ts
@@ -14,15 +14,18 @@ export const getWelcomeEmailHTML = (user: { name: string }) => {
         <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" class="main-box" style="border:3px solid #000; background-color:#f2ecaa;">
           <tr>
             <td align="center" style="padding:40px 0 20px 0;">
-              <h1 style="margin:0; font-weight:900; text-transform:uppercase; letter-spacing:-2px; font-size:48px; color:#000;">ALORA</h1>
+              <h1 style="margin:0; font-weight:900; text-transform:uppercase; letter-spacing:-2px; font-size:48px; color:#000;">
+                <img src="https://aloraa.vercel.app/alora_BG.png" alt="Alora Logo" width="48" height="48" style="vertical-align: middle; margin-right: 10px; display: inline-block;" />
+                <span style="vertical-align: middle;">ALORA</span>
+              </h1>
             </td>
           </tr>
 
           <tr>
             <td align="center" style="padding:0 40px;">
               <div style="width:100%; height:2px; background-color:#000; margin-bottom:30px;"></div>
-              <h2 style="font-size:28px; margin:0; text-transform:uppercase; color:#000;">Welcome to the World of Alora</h2>
-              <p style="font-size:16px; line-height:24px; color:#333; margin:20px 0;">
+              <h2 style="font-size:20px; margin:0; text-transform:uppercase; color:#000;">Welcome to the World of Best Perfumes</h2>
+              <p style="font-size:10px; line-height:24px; color:#333; margin:20px 0;">
                 Hi ${user.name || 'there'}, we're thrilled to have you here. Your journey into the world of fine, signature fragrances starts today.
               </p>
             </td>
@@ -30,7 +33,7 @@ export const getWelcomeEmailHTML = (user: { name: string }) => {
 
           <tr>
             <td align="center" style="padding:30px 0 40px 0;">
-              <a href="https://alora.vercel.app/shop" style="background-color:#000; color:#fff; text-decoration:none; padding:15px 35px; font-weight:bold; text-transform:uppercase; font-size:14px; letter-spacing:1px; display:inline-block;">
+              <a href="https://aloraa.vercel.app/shop" style="background-color:#000; color:#fff; text-decoration:none; padding:15px 35px; font-weight:bold; text-transform:uppercase; font-size:14px; letter-spacing:1px; display:inline-block;">
                 Explore The Collection
               </a>
             </td>
