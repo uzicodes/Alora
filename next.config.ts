cb911df106b23e3d468a5d44c2d7752469bce3f3
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-06-24 19:04:46 +0600

    fix: Clerk auth() not catching proxy.ts so had no server actions on profile page & whitelisted img.clerk.com at next.config

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: "/**",
+      },
     ],
   },
 };
